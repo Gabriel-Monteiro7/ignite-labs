44b128a39d6c021a1305d1ba60848e43c2279cf4
Gabriel-Monteiro7 <gabrielmont713@gmail.com>
2022-07-11 22:16:26 -0300

    feat: adicionando a variante do LessonCard selecionado no storybook

--- src/pages/Event/components/LessonCard/index.stories.tsx
+++ src/pages/Event/components/LessonCard/index.stories.tsx
@@ -1,23 +1,14 @@
 import { add } from 'date-fns'
 
-import LessonCard from '.'
-
 import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { withRouter } from 'storybook-addon-react-router-v6'
+
+import LessonCard from '.'
 
 const StoryLessonCard: ComponentMeta<typeof LessonCard> = {
   title: 'LessonCard',
   component: LessonCard,
-  argTypes: {
-    availableAt: {
-      name: 'availableAt',
-      description: 'Creation date of a lesson.',
-      table: {
-        type: { summary: 'Date.toDateString() result' },
-        defaultValue: { summary: 'Date.now()' }
-      },
-      control: 'date'
-    }
-  }
+  decorators: [withRouter]
 }
 
 const Template: ComponentStory<typeof LessonCard> = (args) => (
@@ -32,6 +23,44 @@ Default.args = {
   slug: 'aula-02-titulo-aula-ignite-labs',
   title: 'Aula 02 - Titulo aula ignite labs',
   lessonType: 'class'
+}
+
+Default.argTypes = {
+  availableAt: {
+    name: 'availableAt',
+    description: 'Creation date of a lesson.',
+    table: {
+      type: { summary: 'Date.toDateString() result' },
+      defaultValue: { summary: 'Date.now()' }
+    },
+    control: 'date'
+  }
+}
+
+export const Selected: ComponentMeta<typeof LessonCard> = Template.bind({})
+
+const currentDay = new Date().toString()
+
+Selected.args = {
+  availableAt: currentDay,
+  slug: 'abertura-do-evento-ignite-labs',
+  title: 'Abertura do evento Ignite labs',
+  lessonType: 'class'
+}
+
+Selected.parameters = {
+  reactRouter: {
+    routePath: '/event/lesson/:slug',
+    routeParams: { slug: 'abertura-do-evento-ignite-labs' }
+  }
+}
+
+Selected.argTypes = {
+  availableAt: {
+    table: {
+      disable: true
+    }
+  }
 }
 
 export default StoryLessonCard
