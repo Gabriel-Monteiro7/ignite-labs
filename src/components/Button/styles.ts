6ede541bdedb8b893ea133c9a33ba0f21e6bd475
Gabriel-Monteiro7 <gabrielmont713@gmail.com>
2022-07-12 21:10:27 -0300

    feat: criando componente de Button

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+const variants = {
+  contained: `bg-green-500 hover:bg-green-700`,
+  outlined: `border-blue-500 border text-blue-500 hover:bg-blue-500 hover:text-gray-900`
+}
+
+interface IContainer {
+  $variant: 'contained' | 'outlined'
+}
+
+export const Container = styled.button.attrs<IContainer>(({ $variant }) => {
+  const variant = variants[$variant]
+
+  const className =
+    'h-14 w-full sm:w-60 flex gap-2 justify-center items-center text-white transition-colors rounded font-semibold uppercase text-sm ' +
+    variant
+
+  return {
+    className
+  }
+})<IContainer>``
+
+export const Title = styled.span.attrs({
+  className: ''
+})``
