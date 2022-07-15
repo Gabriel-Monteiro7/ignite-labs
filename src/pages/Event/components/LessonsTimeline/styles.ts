2ebe696316781f5b78081e8f7faee72c9548a512
Gabriel-Monteiro7 <gabrielmont713@gmail.com>
2022-07-15 16:30:54 -0300

    refactor: adicionando loader no componente SelectedLesson

--- src/pages/Event/components/LessonsTimeline/styles.ts
+++ src/pages/Event/components/LessonsTimeline/styles.ts
@@ -1,8 +1,8 @@
 import styled, { css } from 'styled-components'
 
 export const Container = styled.aside.attrs({
-  className: `bg-gray-700 border-l border-gray-600 w-full h-full absolute
-  lg:w-[348px] lg:relative lg:visible lg:h-auto p-6`
+  className: `bg-gray-700 border-l border-gray-600 w-full h-full z-20 fixed
+  lg:w-[348px] lg:relative lg:visible lg:h-auto p-6 max-h-[92vh] lg:max-h-full overflow-y-auto`
 })<{ $openLessonsTimeline: boolean }>`
   ${({ $openLessonsTimeline }) =>
     !$openLessonsTimeline &&
@@ -14,9 +14,10 @@ export const Container = styled.aside.attrs({
 `
 
 export const Title = styled.h6.attrs({
-  className: 'text-white font-semibold text-2xl pb-6 border-b border-gray-600'
+  className:
+    'text-white font-semibold text-2xl mb-6 pb-6 border-b border-gray-600'
 })``
 
 export const LessonsContainer = styled.div.attrs({
-  className: 'flex flex-col'
+  className: 'flex flex-col gap-6'
 })``
