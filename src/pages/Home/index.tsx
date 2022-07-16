3f7eef0180110f58e916460d9f57d762f5bb5b54
Gabriel-Monteiro7 <gabrielmont713@gmail.com>
2022-07-16 13:24:55 -0300

    feat: iniciando a pagina de Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,39 @@
-import { Container } from './styles'
+import Logo from '~/components/Logo'
+import Footer from '~/components/Footer'
+import Form from './components/Form'
+
+import {
+  Container,
+  Content,
+  Image,
+  Title,
+  ColorfulTitle,
+  LeftContent,
+  RightContent
+} from './styles'
 
 const Home: React.FC = () => {
-  return <Container />
+  return (
+    <>
+      <Container>
+        <Content>
+          <LeftContent>
+            <Logo />
+            <Title>
+              Construa uma <ColorfulTitle>aplicação completa</ColorfulTitle>, do
+              zero, com
+              <ColorfulTitle>React JS</ColorfulTitle>
+            </Title>
+          </LeftContent>
+          <RightContent>
+            <Form />
+          </RightContent>
+        </Content>
+        <Image />
+      </Container>
+      <Footer />
+    </>
+  )
 }
 
 export default Home
--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -1,5 +1,34 @@
 import styled from 'styled-components'
 
+import PrintImage from '~/assets/images/groupMock.png'
+
 export const Container = styled.div.attrs({
-  className: 'bg-gray-900 min-h-screen relative'
+  className:
+    'bg-blur min-h-screen bg-cover bg-no-repeat flex items-center flex-col'
+})``
+
+export const Content = styled.div.attrs({
+  className:
+    'w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto flex-col lg:flex-row'
+})``
+
+export const LeftContent = styled.div.attrs({
+  className: 'md:max-w-[640px]'
+})``
+
+export const RightContent = styled.div.attrs({
+  className: ''
+})``
+
+export const Image = styled.img.attrs({
+  className: 'mt-10',
+  src: PrintImage
+})``
+
+export const Title = styled.h1.attrs({
+  className: 'text-[2.5rem]'
+})``
+
+export const ColorfulTitle = styled.span.attrs({
+  className: 'text-blue-500'
 })``
